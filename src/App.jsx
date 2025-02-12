f7a56aabacac42b1363f6704854f5bdca955b6f9
Valène Renaud <valene.renaud@gmail.com>
2025-02-12 22:53:27 +0100

    chore(project): configure Tailwind CSS and Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 function App() {
-
   return (
     <>
-      <h1>HRnet</h1>
+      <h1 className="text-3xl font-bold underline">HRnet</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
